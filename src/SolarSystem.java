f0e80b22fcf599974bb41e50fa7e21c3fd508087
safinasuhrobi <safina0020@gmail.com>
2022-12-18 12:28:18 -0500

    The basics of solar system are ready

---
+++ src/SolarSystem.java
@@ -0,0 +1,9 @@
+public class SolarSystem {
+    public static void main(String[] args) {
+
+        SolarSystem XX = new SolarSystem();
+        XX.moon = 1;
+        XX.sun = 1;
+        XX.Planet = 2;
+    }
+}
