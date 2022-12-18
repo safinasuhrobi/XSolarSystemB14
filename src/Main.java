27f7554c202025cad023eed0f759acb388215849
safinasuhrobi <safina0020@gmail.com>
2022-12-18 12:41:03 -0500

    Feature sun has been added but it has some errors that need to be resolved

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+        
+            SolarSystem XX = new SolarSystem();
+            XX.moon = 1;
+            XX.sun = 1;
+            XX.Planet = 2;
 
+            FeatureSUN Xeron = new FeatureSUN();
+            Xeron.Heat="12324234";
+            Xeron.size=1221334234;
+            Xeron.Temperature="3434";
 
     }
 }
--- src/SolarSystem.java
+++ src/SolarSystem.java
@@ -1,9 +1,2 @@
 public class SolarSystem {
-    public static void main(String[] args) {
-
-        SolarSystem XX = new SolarSystem();
-        XX.moon = 1;
-        XX.sun = 1;
-        XX.Planet = 2;
-    }
 }
